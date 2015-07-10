445e6751cfc3c61adc2a2aa0dc9420eb4f2e1baf
Theresa Luu <theresa.luu@gmail.com>
2015-07-09 22:15:09 -0700

    cname added

---
+++ vendor/bundle/gems/rdiscount-2.1.7/ext/siteconf20150709-39537-j0gxz6.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150709-39537-lg9uk5"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
